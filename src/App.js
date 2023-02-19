b6a6f82e6d4496cdfb5057fdaba7a5198590d095
sophbradley <111533096+sophbradley@users.noreply.github.com>
2023-02-19 11:58:02 +0000

    Installed moment time zone to retrieve users local time zone, updated defaultCity property in the app file to send the user's location to the weather component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Weather from "./Weather.js";
+import moment from "moment-timezone";
 
 function App() {
+  let localTimeZone = moment.tz.guess();
+  let localCity = localTimeZone.replace("_", " ").split("/")[1];
+
   return (
     <div className="App">
       <div className="container-md">
         <header className="App-header">
           <h1>Weather App</h1>
         </header>
-        <Weather defaultCity="London" />
+        <Weather defaultCity={localCity} />
         <footer className="App-footer">
           <a
             href="https://github.com/sophbradley/weather-app-react"
